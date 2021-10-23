18c394180d8ad05320cda0477352a8a193b9816d
olumide azeez afolabi <midas44000@yahoo.com>
2021-10-23 11:18:36 -0500

    initial commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Fido";
+            int dogAge = 3;
+            Char initial = 'f';
+            bool KnowsTricks = true;
+            double height = 2.5;
+            decimal weight = 36m;
+
+            Console.WriteLine($"My dog's Name is {dogName}. He is {dogAge} years old. it's {initial}. it's {KnowsTricks}");
+            Console.WriteLine($"He is{height} feet tall and weighs {weight} pounds");
+
+
+        }
+    
